73f646fe94f72567eb061de0d8e76a019db66e03
snakeeater00 <snakeeater059@gmail.com>
2026-04-15 13:17:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const buyButtons = document.querySelectorAll('.buy, .ticket-btn');
+document.addEventListener("DOMContentLoaded", () => {
 
-buyButtons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        alert('Билеты скоро будут доступны 🎟️');
+    const buyButtons = document.querySelectorAll('.buy, .ticket-btn');
+
+    buyButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            alert('Билеты скоро будут доступны 🎟️');
+        });
     });
+
+    const trailerBtn = document.querySelector('.trailer');
+
+    if (trailerBtn) {
+        trailerBtn.addEventListener('click', () => {
+            window.open('https://youtu.be/HrgMiZDnEG0?si=MgijTXa7YS2lmkIE', '_blank');
+        });
+    }
+
 });
-
-const trailerBtn = document.querySelector('.trailer');
-
-if (trailerBtn) {
-    trailerBtn.addEventListener('click', () => {
-        window.open('https://youtu.be/HrgMiZDnEG0?si=MgijTXa7YS2lmkIE');
-    });
-}
